e3bdb2dfc7adc447866c909f32e493823c2fe33e
rajshah001 <rajshah30601@gmail.com>
2022-05-12 13:54:47 +0530

    changed packer payload structure

--- generate-packer-script.sh
+++ generate-packer-script.sh
@@ -2,9 +2,6 @@
 
 curl -X POST https://qefflik5hfpyhinjpar63vv5g40fpepo.lambda-url.ap-south-1.on.aws/ -H "Content-Type: application/json" --data-binary @- <<DATA
 {
-  "required": [
-      "nginx-playbook.yml",
-      "php-playbook.yml"
-    ] 
+  "data": ${PACKER_PAYLOAD}
 }
 DATA
