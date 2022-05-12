2e92bf630c8c6775c55c7b1a3d7e97744c5f3504
rajshah001 <rajshah30601@gmail.com>
2022-05-12 20:00:08 +0530

    changed packer payload structure

--- generate-packer-script.sh
+++ generate-packer-script.sh
@@ -2,6 +2,9 @@
 
 curl -X POST https://qefflik5hfpyhinjpar63vv5g40fpepo.lambda-url.ap-south-1.on.aws/ -H "Content-Type: application/json" --data-binary @- <<DATA
 {
-  "data": ${PACKER_PAYLOAD}
+  "data": {
+    "packages": ${PACKAGES},
+    "ami_name": ${AMI_NAME}
+  }
 }
 DATA
