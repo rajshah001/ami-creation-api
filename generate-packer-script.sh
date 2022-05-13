0d2c0fbb0df8d5a9ae4034e1ebed567141068446
rajshah001 <rajshah30601@gmail.com>
2022-05-13 14:24:02 +0530

    debug

--- generate-packer-script.sh
+++ generate-packer-script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 curl -X POST https://qefflik5hfpyhinjpar63vv5g40fpepo.lambda-url.ap-south-1.on.aws/ -H "Content-Type: application/json" --data-binary @- <<DATA
-{
-  "data": {
-    "packages": ${PACKAGES},
-    "ami_name": ${AMI_NAME}
-  }
-}
+{"data":{"packages":${PACKAGES},"ami_name":${AMI_NAME}}}
 DATA
