2b2bb5e6db0ff8017ffcc2cbb31d0e97b6b1e5c3
rajshah001 <rajshah30601@gmail.com>
2022-05-13 15:38:48 +0530

    debug

---
+++ temp.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+package_string='';
+
+input_string="${PACKAGES:1:${#PACKAGES}-2}"
+
+IFS=',' read -ra ADDR <<< "${input_string}"
+for i in "${ADDR[@]}"; do
+  # process "$i"
+  echo $i
+  package_string+=\"$i\"\,
+done
+
+package_string=${package_string%","}
+
+export PACKAGES="[${package_string}]"
+echo "${PACKAGES}"
+echo "${package_string}"
+
+var1=\"${AMI_NAME}\"
+export AMI_NAME=${var1}
+echo "${AMI_NAME}"
