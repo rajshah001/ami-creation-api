376a8e2fa3193b1020c15c4893aea9dc6ef4b190
Raj <33696370+rajshah001@users.noreply.github.com>
2022-05-16 17:26:45 +0530

    added aws UI decryption in comments

--- decrypter.sh
+++ decrypter.sh
@@ -2,3 +2,7 @@ export AZURE_CLIENT_ID=`echo ${AZURE_CLIENT_ID} | rev | base64 -di`
 export AZURE_CLIENT_SECRET=`echo ${AZURE_CLIENT_SECRET} | rev | base64 -di`
 export AZURE_TENANT_ID=`echo ${AZURE_TENANT_ID} | rev | base64 -di`
 export AZURE_SUBSCRIPTION_ID=`echo ${AZURE_SUBSCRIPTION_ID} | rev | base64 -di`
+
+# export AWS_ACCESS_KEY_ID=`echo ${AWS_ACCESS_KEY_ID} | rev | base64 -di`
+# export AWS_SECRET_ACCESS_KEY=`echo ${AWS_SECRET_ACCESS_KEY} | rev | base64 -di`
+# export AWS_ACCOUNT_ID=`echo ${AWS_ACCOUNT_ID} | rev | base64 -di`
