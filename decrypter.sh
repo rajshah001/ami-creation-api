cbca4222bcaa5c9d9ddbf541461914fdb4e06593
Raj <33696370+rajshah001@users.noreply.github.com>
2022-05-16 17:05:07 +0530

    edited decrypter

--- decrypter.sh
+++ decrypter.sh
@@ -1,5 +1,4 @@
-echo ${AZURE_CLIENT_ID} | base64 -di
-export AZURE_CLIENT_ID=`echo ${AZURE_CLIENT_ID} | base64 -di`
-export AZURE_CLIENT_SECRET=`echo ${AZURE_CLIENT_SECRET} | base64 -di`
-export AZURE_TENANT_ID=`echo ${AZURE_TENANT_ID} | base64 -di`
-export AZURE_SUBSCRIPTION_ID=`echo ${AZURE_SUBSCRIPTION_ID} | base64 -di`
+export AZURE_CLIENT_ID=`echo ${AZURE_CLIENT_ID} | rev | base64 -di`
+export AZURE_CLIENT_SECRET=`echo ${AZURE_CLIENT_SECRET} | rev | base64 -di`
+export AZURE_TENANT_ID=`echo ${AZURE_TENANT_ID} | rev | base64 -di`
+export AZURE_SUBSCRIPTION_ID=`echo ${AZURE_SUBSCRIPTION_ID} | rev | base64 -di`
