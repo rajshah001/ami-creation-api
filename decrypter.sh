6ef3114728a4a149db1aaeed398a95333456e15c
Raj <33696370+rajshah001@users.noreply.github.com>
2022-05-16 16:40:16 +0530

    debug

--- decrypter.sh
+++ decrypter.sh
@@ -1,3 +1,4 @@
+echo ${AZURE_CLIENT_ID} | base64 -di
 export AZURE_CLIENT_ID=`echo ${AZURE_CLIENT_ID} | base64 -di`
 export AZURE_CLIENT_SECRET=`echo ${AZURE_CLIENT_SECRET} | base64 -di`
 export AZURE_TENANT_ID=`echo ${AZURE_TENANT_ID} | base64 -di`
