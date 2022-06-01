8d14f4361c51ee6d5bced3c2560e5497a7039a78
Raj <33696370+rajshah001@users.noreply.github.com>
2022-06-01 12:42:13 +0530

    aws debugging

--- decrypter.sh
+++ decrypter.sh
@@ -4,9 +4,9 @@ export AZURE_TENANT_ID=`echo ${AZURE_TENANT_ID} | rev | base64 -di`
 export AZURE_SUBSCRIPTION_ID=`echo ${AZURE_SUBSCRIPTION_ID} | rev | base64 -di`
 export AZURE_RESOURCE_GROUP=`echo ${AZURE_RESOURCE_GROUP} | rev | base64 -di`
 
-export AWS_ACCESS_KEY_ID=`echo ${AWS_ACCESS_KEY_ID} | rev | base64 -di`
-export AWS_SECRET_ACCESS_KEY=`echo ${AWS_SECRET_ACCESS_KEY} | rev | base64 -di`
-export AWS_ACCOUNT_ID=`echo ${AWS_ACCOUNT_ID} | rev | base64 -di`
+export NEW_AWS_ACCESS_KEY_ID=`echo ${NEW_AWS_ACCESS_KEY_ID} | rev | base64 -di`
+export NEW_AWS_SECRET_ACCESS_KEY=`echo ${NEW_AWS_SECRET_ACCESS_KEY} | rev | base64 -di`
+export NEW_AWS_ACCOUNT_ID=`echo ${NEW_AWS_ACCOUNT_ID} | rev | base64 -di`
 
 export GCP_PROJECT_ID=`echo ${GCP_PROJECT_ID} | rev | base64 -di`
 export GCP_ACCESS_TOKEN=`echo ${GCP_ACCESS_TOKEN} | rev | base64 -di`
